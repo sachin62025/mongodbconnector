c2ab16840e474b7b96b526196b59ec29d9619ad7
Sachin Kumar <sachinkumar18449@iiitmanipur.ac.in>
2024-08-24 19:46:08 +0530

    updated

--- setup.py
+++ setup.py
@@ -5,27 +5,24 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+_version_ = "0.0.5"
+REPO_NAME = "mongodbconnector"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "sachin62025"
+AUTHOR_EMAIL = "sachinkumar18449@iiitmanipur.ac.in"
 
 setup(
     name=PKG_NAME,
-    version=__version__,
-    author=sachin62025,
-    author_email=sachinkumar18499@giiitmanipur.ac.in,
+    version=_version_,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/{sachin62025}/{mongodbconnector}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"https://github.com/{sachin62025}/{mongodbconnector}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     )
-
-
-
