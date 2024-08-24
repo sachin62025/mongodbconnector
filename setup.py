7e0aebda7ebe9a7e1003e5c613b1c1b25c77a693
Sachin Kumar <sachinkumar18449@iiitmanipur.ac.in>
2024-08-24 22:58:59 +0530

    RE-DONE

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-_version_ = "0.0.5"
+_version_ = "0.0.6"
 REPO_NAME = "mongodbconnector"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "sachin62025"
